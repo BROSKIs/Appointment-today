41b8e032bf7b71b2b48be515550b1f89b18b7e6c
SamuelM007 <174657033+SamuelM007@users.noreply.github.com>
2026-02-10 15:19:23 -0800

    did some stuff, still doesnt work

--- app.js
+++ app.js
@@ -4,18 +4,31 @@ const app = express();
 
 const PORT = 3000;
 
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+
+const appointments = [];
+
 app.get("/", (req, res) => {
   res.sendFile(`${import.meta.dirname}/views/home.html`);
 });
 
+app.get("/admin", (req, res) => {
+  res.send(appointments);
+});
+
 app.post("/submit", (req, res) => {
-  const schedule = {
+  const appointment = {
     fname: req.body.fname,
     lname: req.body.lame,
     date: req.body.date,
     time: req.body.time,
     timestamp: new Date(),
   };
+
+  appointments.push(appointment);
+  res.send(appointments);
 });
 
 app.listen(PORT, () => {
